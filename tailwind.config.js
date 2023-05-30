8e20abb9dca4856aadf205cbe58adc3b2cb0c4ee
Nick <nicknanos7@gmail.com>
2023-05-30 21:08:26 +0300

    Fix Logo/ Add form

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         'hero-image': "url('https://images.unsplash.com/photo-1585202900225-6d3ac20a6962?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80')",
+        //'logo': "url('https://cdn.statically.io/gh/TheOdinProject/curriculum/5f37d43908ef92499e95a9b90fc3cc291a95014c/html_css/project-sign-up-form/odin-lined.png')",
+      },
+      fontFamily: {
+        'body': ['"Ubuntu Mono"', 'ui-sans-serif'],
       }
     },
   },
