14647e35f1b81fc4fcb5af92a95c961e5db21eb2
Nick <nicknanos7@gmail.com>
2023-06-01 22:43:11 +0300

    Change page art

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./docs/**/*.{html,js}"],
   theme: {
     extend: {
       backgroundImage: {
-        'hero-image': "url('https://images.unsplash.com/photo-1585202900225-6d3ac20a6962?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80')",
+        'hero-image': "url('../img/image.png')",
         //'logo': "url('https://cdn.statically.io/gh/TheOdinProject/curriculum/5f37d43908ef92499e95a9b90fc3cc291a95014c/html_css/project-sign-up-form/odin-lined.png')",
       },
       fontFamily: {
         'body': ['"Ubuntu Mono"', 'ui-sans-serif'],
-      }
+        'logo': ['"Montserrat"', 'ui-sans-serif'],
+      },
+      colors: {
+        'myBlue': '#222631',
+        'myYellow': '#f4aa07',
+      },
     },
   },
   plugins: [],
