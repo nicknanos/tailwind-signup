81515777e282f60d6b22ffb1cd961227609be1be
Nick <nicknanos7@gmail.com>
2023-06-04 16:00:39 +0300

    Use Prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,18 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero-image': "url('../img/image.png')",
+        "hero-image": "url('../img/image.png')",
         //'logo': "url('https://cdn.statically.io/gh/TheOdinProject/curriculum/5f37d43908ef92499e95a9b90fc3cc291a95014c/html_css/project-sign-up-form/odin-lined.png')",
       },
       fontFamily: {
-        'body': ['"Ubuntu Mono"', 'ui-sans-serif'],
-        'logo': ['"Montserrat"', 'ui-sans-serif'],
+        body: ['"Ubuntu Mono"', "ui-sans-serif"],
+        logo: ['"Montserrat"', "ui-sans-serif"],
       },
       colors: {
-        'myBlue': '#222631',
-        'myYellow': '#f4aa07',
+        myBlue: "#222631",
+        myYellow: "#f4aa07",
       },
     },
   },
   plugins: [],
-}
+};
